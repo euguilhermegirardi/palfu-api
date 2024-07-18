cbdc1c2136950a741c16df0e1ec89b23a708bcfd
euguilhermegirardi <girardi.gui@icloud.com>
2024-07-18 13:12:54 -0300

    fix

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express');
 const cors = require('cors');
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(cors({
-  origin: 'https://palfu.netlify.app/',
-}));
+
+server.use(cors());
+
 server.use('/api', router);
-server.options('/api/list', cors());
 
 server.listen(process.env.PORT || 5000, () => {
   console.log("JSON Server is running");
